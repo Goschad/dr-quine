3996aaf589a453923205d0c36bb779015631f048
Marwen Bouaza <mbouaza@k2r1p3.42mulhouse.fr>
2024-05-31 17:15:26 +0200

    Python is true

--- Python/Grace.py
+++ Python/Grace.py
@@ -2,9 +2,15 @@
     out
 '''
 
+def D0(x):
+    return x
+
+def D1(x):
+    return x
+
 def FT(x):
     f = open('Grace_kid.py', 'w')
-    a = "'''\n    out\n'''\n\ndef FT(x):\n    f = open('Grace_kid.py', 'w')\n    a = %r\n    f.write(a%%a)\n    f.close()\n\nFT(0)\n"
+    a = "'''\n    out\n'''\n\ndef D0(x):\n    return x\n\ndef D1(x):\n    return x\n\ndef FT(x):\n    f = open('Grace_kid.py', 'w')\n    a = %r\n    f.write(a%%a)\n    f.close()\n\nFT(0)\n"
     f.write(a%a)
     f.close()
 
--- Python/Sully.py
+++ Python/Sully.py
@@ -1,14 +1,18 @@
 index = 5
 
-import subprocess, sys
+import subprocess, sys, os
 
 if __name__ == '__main__':
     if index - 1 < 0:
         sys.exit()
-    f = open('Sully_' + str(index - 1) + '.py', 'w')
-    i = 'index = ' + str(index - 1)
-    a = "\n\nimport subprocess, sys\n\nif __name__ == '__main__':\n    if index - 1 < 0:\n        sys.exit()\n    f = open('Sully_' + str(index - 1) + '.py', 'w')\n    i = 'index = ' + str(index - 1)\n    a = %r\n    f.write(i + a%%a)\n    f.close()\n    subprocess.run('python3 Sully_' + str(index - 1) + '.py', shell = True, executable='/bin/bash')\n    sys.exit()"
+    path = os.path.abspath(__file__)
+    n_file = os.path.basename(path)
+    if n_file != 'Sully.py':
+        index -= 1
+    f = open('Sully_' + str(index) + '.py', 'w')
+    i = 'index = ' + str(index)
+    a = "\n\nimport subprocess, sys, os\n\nif __name__ == '__main__':\n    if index - 1 < 0:\n        sys.exit()\n    path = os.path.abspath(__file__)\n    n_file = os.path.basename(path)\n    if n_file != 'Sully.py':\n        index -= 1\n    f = open('Sully_' + str(index) + '.py', 'w')\n    i = 'index = ' + str(index)\n    a = %r\n    f.write(i + a%%a)\n    f.close()\n    subprocess.run('python3 Sully_' + str(index) + '.py', shell = True, executable='/bin/bash')\n    sys.exit()"
     f.write(i + a%a)
     f.close()
-    subprocess.run('python3 Sully_' + str(index - 1) + '.py', shell = True, executable='/bin/bash')
+    subprocess.run('python3 Sully_' + str(index) + '.py', shell = True, executable='/bin/bash')
     sys.exit()
